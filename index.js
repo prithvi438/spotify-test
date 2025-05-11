9c09534c8669ceb1bd7af09dfc7309249ad741a4
prithvi438 <137283892+prithvi438@users.noreply.github.com>
2025-05-11 11:56:09 +0530

    changes

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 import express from 'express';
 
 const app = express();
+
+app.listen(5200, () => {
+    console.log('App listening at port 8000');
+});
+
+
+app.get("/", async (req, res) => {
+    res.json({
+        "data": "this is test data"
+    })
+});
+
